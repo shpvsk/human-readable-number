4f22a0dd61b2b6a9fbf7a47eaa2834fb72487ab3
shpvsk <mshpvsk@yandex.ru>
2022-06-24 22:08:42 +0300

    feat: add return digits up to 20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const numbersArrayUpToTwenty  = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    const dozensArr = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+
+    if (number < 20) return numbersArrayUpToTwenty[number]
+
 }

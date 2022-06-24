af4cc9fc32e8ababca20cfdf69ed96a73eb0b4e4
shpvsk <mshpvsk@yandex.ru>
2022-06-24 22:09:39 +0300

    feat: add return digits up to 100

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ module.exports = function toReadable(number) {
     const numbersArrayUpToTwenty  = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
     const dozensArr = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
+    const str = String(number)
+
 
     if (number < 20) return numbersArrayUpToTwenty[number]
+
+    if (number >= 20 && number < 100) {
+        if (str.slice(-1) != 0) {
+            return `${dozensArr[str[0]]} ${numbersArrayUpToTwenty[str[1]]}`
+        } else {
+            return dozensArr[str[0]]
+        }
+    }
 
 }
